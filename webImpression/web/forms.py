6878aaac8f0ec453a71745ce9af7de86e65205de
ramonaweb2 <ramona.gospodinova@plana.solutions>
2023-07-01 20:42:53 +0300

    update settings and README.md

--- webImpression/web/forms.py
+++ webImpression/web/forms.py
@@ -1,12 +1,13 @@
+
 from django import forms
 from django.core.validators import EmailValidator
 
 
 class ContactForm(forms.Form):
 
-    first_name = forms.CharField(max_length=50)
-    last_name = forms.CharField(max_length=50)
-    email = forms.EmailField(max_length=50, validators=[EmailValidator])
-    subject = forms.CharField(max_length=50)
-    message = forms.CharField(widget=forms.Textarea)
+    first_name = forms.CharField(max_length=50, label='Име')
+    last_name = forms.CharField(max_length=50, label='Фамилия')
+    email = forms.EmailField(max_length=50, validators=[EmailValidator], label='Имейл')
+    subject = forms.CharField(max_length=50, label='Тема')
+    message = forms.CharField(widget=forms.Textarea, label='Съобщение')
 
