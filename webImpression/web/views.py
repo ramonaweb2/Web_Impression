96a78f38d0f45209cfc9454e971dd53af8bdcaa9
ramonaweb2 <ramona.gospodinova@plana.solutions>
2023-07-02 15:41:55 +0300

    using Tools for sending email (send_email)

--- webImpression/web/views.py
+++ webImpression/web/views.py
@@ -2,11 +2,12 @@ from smtplib import SMTPDataError
 
 from django.conf import settings
 
-from django.core.mail import EmailMessage, get_connection
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.views import View
 
 from webImpression.web.forms import ContactForm
+
+from django.core.mail import send_mail
 
 
 class HomePageView(View):
@@ -34,27 +35,17 @@ class HomePageView(View):
             subject = form.cleaned_data.get('subject')
             message = form.cleaned_data.get('message')
 
-            with get_connection(
-                host=settings.EMAIL_HOST,
-                port=settings.EMAIL_PORT,
-                username=settings.EMAIL_HOST_USER,
-                password=settings.EMAIL_HOST_PASSWORD,
-                use_tls=settings.EMAIL_USE_TLS
-            ) as connection:
-                    email = EmailMessage(
-                        subject=subject,
-                        body=message,
-                        from_email='info@web-impression.net',
-                        to=['ramona.gospodinova@gmail.com'],
-                        reply_to=[email],    # Email from the form to get back to,
-                        connection=connection
-                    )
-                    try:
-                        email.send()
-                    except SMTPDataError as error:
-                        context.update({
-                            'message': error.args[1],
-                        })
+            try:
+                send_mail(
+                    subject,
+                    message,
+                    from_email=settings.DEFAULT_FROM_EMAIL,
+                    recipient_list=[email],
+                    fail_silently=False)
+            except SMTPDataError as error:
+                context.update({
+                    'message': error.args[1],
+                })
 
         return render(request, 'index.html', context)
 
