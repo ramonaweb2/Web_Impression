baf2697fe62197a80cafab9e9c2352946b503345
ramonaweb2 <ramona.gospodinova@plana.solutions>
2023-07-09 23:30:02 +0300

    contact receive message

--- webImpression/web/views.py
+++ webImpression/web/views.py
@@ -73,3 +73,38 @@ class ContactsView(View):
             'form': form,
         }
         return render(request, 'contacts.html', context)
+
+    @staticmethod
+    def post(request):
+        form = ContactForm(request.POST)
+
+        context = {
+            'form': form,
+        }
+        if form.is_valid():
+            first_name = form.cleaned_data.get('first_name')
+            last_name = form.cleaned_data.get('last_name')
+            email_from = form.cleaned_data.get('email')
+            subject = form.cleaned_data.get('subject')
+            form_message = form.cleaned_data.get('message')
+
+            message = f"<div>Message: {form_message}</div>"
+            message += f"<div>From email: {email_from}</div>"
+            message += f"<div>Name: {first_name} {last_name}</div>"
+
+            try:
+                status_code = send_email_message(subject, message)
+                if status_code:
+                    message_success = 'Email sent successfully'
+                else:
+                    message_success = 'Email not sent successfully'
+
+                context.update({
+                    'message_success': message_success
+                })
+            except SMTPDataError as error:
+                context.update({
+                    'message_errors': error.args[1],
+                })
+
+        return render(request, 'contacts.html', context)
