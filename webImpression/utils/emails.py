43c690f13bf6d05726aed91b987338c6e0e5b8ca
ramonaweb2 <ramona.gospodinova@plana.solutions>
2023-07-10 21:49:13 +0300

    contact receive message

--- webImpression/utils/emails.py
+++ webImpression/utils/emails.py
@@ -1,8 +1,12 @@
 """
 This call sends a message to one recipient.
 """
+from smtplib import SMTPDataError
+
 from mailjet_rest import Client
 from django.conf import settings
+
+from webImpression.web.forms import ContactForm
 
 DEFAULT_FROM_EMAIL = settings.DEFAULT_FROM_EMAIL
 DEFAULT_TO_EMAIL = settings.DEFAULT_TO_EMAIL
@@ -10,7 +14,41 @@ API_KEY = settings.MAILJET_API_KEY
 API_SECRET = settings.MAILJET_API_SECRET
 
 
-def send_email_message(subject, message):
+def send_email(request):
+    form = ContactForm(request.POST)
+
+    context = {
+        'form': form,
+    }
+    if form.is_valid():
+        first_name = form.cleaned_data.get('first_name')
+        last_name = form.cleaned_data.get('last_name')
+        email_from = form.cleaned_data.get('email')
+        subject = form.cleaned_data.get('subject')
+        form_message = form.cleaned_data.get('message')
+
+        message = f"<div>Message: {form_message}</div>"
+        message += f"<div>From email: {email_from}</div>"
+        message += f"<div>Name: {first_name} {last_name}</div>"
+
+        message_success = 'Email sent successfully'
+
+        try:
+            status_code = compose_email_message(subject, message)
+            if not status_code:
+                message_success = 'Email not sent successfully'
+
+        except SMTPDataError as error:
+            context.update({
+                'message_errors': error.args[1],
+            })
+    else:
+        message_success = 'Email not sent successfully'
+
+    return message_success
+
+
+def compose_email_message(subject, message):
     mailjet = Client(auth=(API_KEY, API_SECRET), version='v3.1')
 
     data = {
--- webImpression/web/views.py
+++ webImpression/web/views.py
@@ -1,10 +1,8 @@
-from smtplib import SMTPDataError
-
 
 from django.shortcuts import render
 from django.views import View
 
-from webImpression.utils.emails import send_email_message
+from webImpression.utils.emails import send_email
 from webImpression.web.forms import ContactForm
 
 
@@ -24,37 +22,13 @@ class HomePageView(View):
 
     @staticmethod
     def post(request):
+        message_success = send_email(request)
         form = ContactForm(request.POST)
 
         context = {
             'form': form,
+            'message_success': message_success,
         }
-        if form.is_valid():
-            first_name = form.cleaned_data.get('first_name')
-            last_name = form.cleaned_data.get('last_name')
-            email_from = form.cleaned_data.get('email')
-            subject = form.cleaned_data.get('subject')
-            form_message = form.cleaned_data.get('message')
-
-            message = f"<div>Message: {form_message}</div>"
-            message += f"<div>From email: {email_from}</div>"
-            message += f"<div>Name: {first_name} {last_name}</div>"
-
-            try:
-                status_code = send_email_message(subject, message)
-                if status_code:
-                    message_success = 'Email sent successfully'
-                else:
-                    message_success = 'Email not sent successfully'
-
-                context.update({
-                    'message_success': message_success
-                })
-            except SMTPDataError as error:
-                context.update({
-                    'message_errors': error.args[1],
-                })
-
         return render(request, 'index.html', context)
 
 
@@ -76,35 +50,11 @@ class ContactsView(View):
 
     @staticmethod
     def post(request):
+        message_success = send_email(request)
         form = ContactForm(request.POST)
 
         context = {
             'form': form,
+            'message_success': message_success,
         }
-        if form.is_valid():
-            first_name = form.cleaned_data.get('first_name')
-            last_name = form.cleaned_data.get('last_name')
-            email_from = form.cleaned_data.get('email')
-            subject = form.cleaned_data.get('subject')
-            form_message = form.cleaned_data.get('message')
-
-            message = f"<div>Message: {form_message}</div>"
-            message += f"<div>From email: {email_from}</div>"
-            message += f"<div>Name: {first_name} {last_name}</div>"
-
-            try:
-                status_code = send_email_message(subject, message)
-                if status_code:
-                    message_success = 'Email sent successfully'
-                else:
-                    message_success = 'Email not sent successfully'
-
-                context.update({
-                    'message_success': message_success
-                })
-            except SMTPDataError as error:
-                context.update({
-                    'message_errors': error.args[1],
-                })
-
         return render(request, 'contacts.html', context)
